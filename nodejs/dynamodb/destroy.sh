342e21ca1530b7a31c9b6e1194407f07cf3ce42b
Allan Chua <allanchua.officefiles@gmail.com>
2022-10-09 00:52:24 +0800

    Cleanup

--- nodejs/dynamodb/destroy.sh
+++ nodejs/dynamodb/destroy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SAM_BUCKET_POC="ac-sam-bucket-pocs"; # Put your bucket name here
-AWS_CLI_PROFILE_NAME="my-poc-profile";
+SAM_BUCKET_POC=""; # Put your bucket name here
+AWS_CLI_PROFILE_NAME="";
 
 sam delete --stack-name "dynamo-connector-poc" \
   --profile $AWS_CLI_PROFILE_NAME \
